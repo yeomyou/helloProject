3bcb15a9d7fd261096cc9600069964b9c7556aa0
admin <admin@DESKTOP-BQ4LMLA>
2023-05-30 12:31:49 +0900

    a

---
+++ src/com/yedam/operator/OperatorEx1.java
@@ -0,0 +1,12 @@
+package com.yedam.operator;
+
+import java.util.Scanner;
+
+public class OperatorEx1 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("숫자를 입력하세요,");
+		scn.nextInt();
+		scn.close();
+	}
+}
