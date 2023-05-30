bc757d469f41625c3160e98eaf93dcac00a6c7cc
admin <admin@DESKTOP-BQ4LMLA>
2023-05-30 12:33:07 +0900

    asdasf

--- src/com/yedam/operator/OperatorEx1.java
+++ src/com/yedam/operator/OperatorEx1.java
@@ -6,6 +6,7 @@ public class OperatorEx1 {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		System.out.println("숫자를 입력하세요,");
+		System.out.println("숫자를 입력하세요,");
 		scn.nextInt();
 		scn.close();
 	}
